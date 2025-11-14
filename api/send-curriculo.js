d82304de1f7f88a8ab2a6d84cb6dffc50c5eb6ba
Anderson Vieira <avieira@up-ti.com>
2025-11-14 18:25:37 -0300

    wip

--- api/send-curriculo.js
+++ api/send-curriculo.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT),
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
